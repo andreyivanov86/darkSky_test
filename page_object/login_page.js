eadeb863e4aec093b7c20375601f5201730979dd
AndreyI <andreyivanov86@gmail.com>
2019-11-25 23:08:41 -0500

    corrected loginOperation and test for required fields using <input> tag

--- page_object/login_page.js
+++ page_object/login_page.js
@@ -2,11 +2,20 @@ class Login {
   get loginEmail() { return $('#email'); }
   get loginPassword() { return $('#password'); }
   get loginButton() { return $('button'); }
+
   loginOperaion(email, password) {
-    this.loginEmail.setValue(email);
-    this.loginPassword.setValue(password);
+    if (email) {
+      this.loginEmail.setValue(email);
+    } else {
+      this.loginEmail.setValue("");
+    }
+    if (password) {
+      this.loginPassword.setValue(password);
+    } else {
+      this.loginPassword.setValue("");
+    }
     this.loginButton.click();
   }
-}
 
+}
 module.exports = new Login();
--- test/login_test.js
+++ test/login_test.js
@@ -1,31 +1,33 @@
 //login page
-  //should be able to land on login page
-  //should be able to login
-  //should error if no username provided
-  //should error if username format is incorrect
-  //should error if no password provided
-  //should error if username or password is incorrect
+//should be able to land on login page
+//should be able to login
+//should require email as username
+//should error if username or password is incorrect
 
-  let login = require('../page_object/login_page.js');
-  let users = require('../test_data/login_data.js')
+let login = require('../page_object/login_page.js');
+let users = require('../test_data/login_data.js')
 
 
-  before(() => {
+describe('login page', () => {
+  beforeEach(() => {
     browser.url('/dev/login');
   });
-
-  describe('login page', () => {
-    it('should be able to land on login page', () => {
-      expect(login.loginEmail.isVisible()).to.be.true;
-      expect(login.loginPassword.isVisible()).to.be.true;
-      expect(login.loginButton.isVisible()).to.be.true;
-    });
-    it('should be able to login', () => {
-      login.loginOperaion(users.user1.email, users.user1.password);
-      expect(browser.getUrl()).to.equal('https://darksky.net/dev/account');
-    });
-    it('should be able to login', () => {
-      // login.loginOperaion(users.user1.email, users.user1.password);
-      // expect(browser.getUrl()).to.equal('https://darksky.net/dev/account');
-    });
+  it('should be able to land on login page', () => {
+    expect(login.loginEmail.isVisible()).to.be.true;
+    expect(login.loginPassword.isVisible()).to.be.true;
+    expect(login.loginButton.isVisible()).to.be.true;
   });
+  it('should be able to login', () => {
+    login.loginOperaion(users.user1.email, users.user1.password);
+    expect(browser.getUrl()).to.equal('https://darksky.net/dev/account');
+  });
+  it('should require email as username and password', () => {
+    expect(login.loginEmail.getAttribute('type')).to.equal('email');
+    expect(login.loginEmail.getAttribute('required')).to.equal('true');
+    expect(login.loginPassword.getAttribute('required')).to.equal('true');
+  });
+    //TODO add it block for the case when credentials are incorrect
+});
+
+
+
